ac9353c2c2f47edb588cc80b135912235be9a7fa
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-02-28 22:04:06 +0530

    Initial commit

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+window.onload = function()
+{
+    var bsDiv= document.getElementById('box-shadow-div');
+    var x,y;
+
+    window.addEventListener('mouseover', function(event)
+    {
+        x = event.clientX;
+        y = event.clientY;
+        if (typeof x !== 'undefined')
+        {
+bsDiv.style.left = x - bsDiv.clientWidth/2 + 'px';
+bsDiv.style.top = y - bsDiv.clientHeight/2 + 'px';
+
+        }
+    },
+    false
+    );
+};
